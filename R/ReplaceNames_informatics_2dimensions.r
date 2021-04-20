a97231417f9b8b5330fdee5cbecf5b8eac2eac4f
WNF <william@fluxrme.com>
2021-04-19 19:52:33 -0500

    Updated ReplaceNames with tb

--- R/ReplaceNames_informatics_2dimensions.r
+++ R/ReplaceNames_informatics_2dimensions.r
@@ -1,16 +1,16 @@
 #' ReplaceNames 
 #'
 #' Replace selected names the names in a data frame or tibble.
-#' @param df Table where names will be replaced. Must be data frame or tibble.
+#' @param tb Table where names will be replaced. Must be data frame or tibble.
 #' @param current.names Names to be replaced. Must be a character string.
 #' @param new.names New names that will be inserted in the place of current.names. Must be a character string.
 #' @return R data table or tibble object with different column names (replaces R object with original names)
 #' @export
 
-ReplaceNames <- function(df,current.names, new.names) {
+ReplaceNames <- function(tb,current.names, new.names) {
 
       #Data Checks
-      if(!is.data.frame(df)){
+      if(!is.data.frame(tb)){
         stop("Input not a data frame. Input must be of class 'data.frame'.")
       }
 
@@ -32,16 +32,16 @@ ReplaceNames <- function(df,current.names, new.names) {
       #Current Names Checks
       if(!exists("current.names")){
 
-        if(length(names(df)) == length(new.names)){
+        if(length(names(tb)) == length(new.names)){
           print("No current names to replace specified. All current names will be replaced.")
-          current.names <- names(df)
+          current.names <- names(tb)
         }
 
-        if(length(names(df)) != length(new.names)){
+        if(length(names(tb)) != length(new.names)){
           stop(
             paste(
-              "No current names to replace specified. Current df has ",
-              length(names(df)),
+              "No current names to replace specified. Current tb has ",
+              length(names(tb)),
               " columns. New names is of length ",
               length(new.names),
               ".",
@@ -52,11 +52,11 @@ ReplaceNames <- function(df,current.names, new.names) {
 
       } #End of if statement for when current.names not defined by user
 
-      if(any(!current.names %in% names(df))){
+      if(any(!current.names %in% names(tb))){
         warning(
           paste(
             "One or more current.names were not found in input data frame: '",
-            current.names[!current.names %in% names(df)],
+            current.names[!current.names %in% names(tb)],
             "'. ",
             sep = ""
           )
@@ -64,6 +64,6 @@ ReplaceNames <- function(df,current.names, new.names) {
       }
 
       #Actual Function: name replacement
-      names(df)[names(df) %in% current.names] <- new.names
-      return(df)
+      names(tb)[names(tb) %in% current.names] <- new.names
+      return(tb)
     }
